6a8a157e08ecec70302a678a7b85c0ceebfb03ac
p18an1v <pranavmhargude@gmail.com>
2024-07-03 18:25:07 +0530

    u: update the view port problem

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,15 +6,15 @@ import ConnectBTN from "./ConnectBTN";
 const Hero = ({ scrollContainer }) => {
   return (
     <section className="parallax">
-      <div className='parallax__content absolute top-[10%] sm:top-[16%] lg:top-[24%] w-full mx-auto lg:pl-[10%] lg:pr-[10%] xl:pl-[15%] xl:pr-[15%] 2xl:px-[10%] 3xl:px-[10%] flex flex-col lg:flex-row items-start z-10' style={{ zIndex: 500 }}>
-        <div className="flex-1 lg:mb-0">
-          <h1 className='font-medium text-white text-[40px] xs:text-[50px] sm:text-[68px] md:text-[80px] lg:text-[100px] 2xl:text-[140px] leading-[110px] 2xl:leading-[160px]'>
+      <div className='parallax__content absolute top-[10%] sm:top-[16%] lg:top-[18%] w-full mx-auto lg:pl-[10%] lg:pr-[10%] xl:pl-[15%] xl:pr-[15%] 2xl:px-[10%] 3xl:px-[10%] flex flex-col lg:flex-row items-start z-10' style={{ zIndex: 500 }}>
+        <div className="flex-1 lg:mb-0 ml-4 lg:ml-10 xl:ml-20">
+          <h1 className='font-medium text-white text-[40px] xs:text-[50px] sm:text-[68px] md:text-[80px] lg:text-[200px] 2xl:text-[140px] leading-[110px] 2xl:leading-[160px]'>
             Pranav Mhargude 
           </h1>
           <br/>
           <Position />
         </div>
-        <div className="flex-1 flex justify-start lg:justify-end mt-4 sm:mt-14 ml-2 xs:ml-[-2vh] sm:ml-[-10vh] md:ml-[-15vh] lg:mt-10 2xl:mt-0" style={{ zIndex: 1000 }}>
+        <div className="flex-1 flex justify-start lg:justify-end mt-4 sm:mt-14 mr-4 lg:mr-10 xl:mr-20 xs:ml-[-2vh] sm:ml-[-10vh] md:ml-[-15vh] lg:mt-10 2xl:mt-0" style={{ zIndex: 1000 }}>
           <div className='font-bold text-[20px] sm:text-[30px] md:text-[36px] 2xl:text-[46px] sm:leading-[40px] md:leading-[50px] 2xl:leading-[60px] streaky-glow max-w-sm 2xl:max-w-lg text-white text-left' style={{ zIndex: 1000 }}>
             <ConnectBTN />
           </div>
@@ -34,4 +34,5 @@ const Hero = ({ scrollContainer }) => {
 };
 
 export default Hero;
+
 
